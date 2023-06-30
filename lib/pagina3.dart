e9dbb9480a8e1255dff4d123da91b1d8c524288e
DevMaic <maic1405@gmail.com>
2023-06-30 10:37:12 -0300

    homepage concluída, navegação para a página 2 funcional

---
+++ lib/pagina3.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+    runApp(Pagina3());
+}
+
+class Pagina3 extends StatelessWidget {
+  const Pagina3({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
